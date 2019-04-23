f04fdff86d64efbb381992fd363a8e242c965127
David P. Riedel <davidp.riedel@gmail.com>
2019-04-23 09:37:03 -0400

    split out some utility routines from Extractor
    - boost assertion code
    - assertion exception
    - catenate code

---
+++ src/Collector_Utils.cpp
@@ -0,0 +1,86 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Collector_Utils.cpp
+ *
+ *    Description:  Useful routines from Extractor code base
+ *
+ *        Version:  1.0
+ *        Created:  04/22/2019 03:03:20 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David P. Riedel (), driedel@cox.net
+ *        License:  GNU General Public License V3
+ *
+ * =====================================================================================
+ */
+
+	/* This file is part of Collector. */
+
+	/* Collector is free software: you can redistribute it and/or modify */
+	/* it under the terms of the GNU General Public License as published by */
+	/* the Free Software Foundation, either version 3 of the License, or */
+	/* (at your option) any later version. */
+
+	/* Collector is distributed in the hope that it will be useful, */
+	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+	/* GNU General Public License for more details. */
+
+	/* You should have received a copy of the GNU General Public License */
+	/* along with Collector.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include "Collector_Utils.h"
+
+
+/*
+ *--------------------------------------------------------------------------------------
+ *       Class:  AssertionException
+ *      Method:  AssertionException
+ * Description:  constructor
+ *--------------------------------------------------------------------------------------
+ */
+AssertionException::AssertionException(const char* text)
+    : std::invalid_argument(text)
+{
+}  /* -----  end of method AssertionException::AssertionException  (constructor)  ----- */
+
+AssertionException::AssertionException(const std::string& text)
+    : std::invalid_argument(text)
+{
+}  /* -----  end of method AssertionException::AssertionException  (constructor)  ----- */
+
+
+namespace boost
+{
+    // these functions are declared in the library headers but left to the user to define.
+    // so here they are...
+    //
+    /* 
+     * ===  FUNCTION  ======================================================================
+     *         Name:  assertion_failed_mgs
+     *  Description:  
+     *         defined in boost header but left to us to implement.
+     * =====================================================================================
+     */
+
+    void assertion_failed_msg (char const* expr, char const* msg, char const* function, char const* file, long line)
+    {
+        throw AssertionException(catenate("\n*** Assertion failed *** test: ", expr, " in function: ", function,
+                    " from file: ", file, " at line: ", line, ".\nassertion msg: ", msg));
+    }		/* -----  end of function assertion_failed_mgs  ----- */
+
+    /* 
+     * ===  FUNCTION  ======================================================================
+     *         Name:  assertion_failed
+     *  Description:  
+     * =====================================================================================
+     */
+    void assertion_failed (char const* expr, char const* function, char const* file, long line )
+    {
+        throw AssertionException(catenate("\n*** Assertion failed *** test: ", expr, " in function: ", function,
+                    " from file: ", file, " at line: ", line));
+    }		/* -----  end of function assertion_failed  ----- */
+
+}		/* -----  end of namespace boost  ----- */
---
+++ src/Collector_Utils.h
@@ -0,0 +1,104 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Collector_Utils.h
+ *
+ *    Description:  some shared routines
+ *
+ *        Version:  1.0
+ *        Created:  04/22/2019 02:14:42 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  David P. Riedel (dpr), driedel@cox.net
+ *        License:  GNU General Public License v3
+ *        Company:
+ *
+ * =====================================================================================
+ */
+
+	/* This file is part of Collector. */
+
+	/* Collector is free software: you can redistribute it and/or modify */
+	/* it under the terms of the GNU General Public License as published by */
+	/* the Free Software Foundation, either version 3 of the License, or */
+	/* (at your option) any later version. */
+
+	/* Collector is distributed in the hope that it will be useful, */
+	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+	/* GNU General Public License for more details. */
+
+	/* You should have received a copy of the GNU General Public License */
+	/* along with Collector.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef  _COLLECTOR_UTILS_INC_
+#define  _COLLECTOR_UTILS_INC_
+
+#include <exception>
+#include <functional>
+#include <string>
+#include <type_traits>
+
+#include <boost/assert.hpp>
+#include <boost/mp11.hpp>
+
+namespace mp11 = boost::mp11;
+
+class AssertionException : public std::invalid_argument
+{
+public:
+
+    explicit AssertionException(const char* what);
+
+    explicit AssertionException(const std::string& what);
+};
+
+
+// some code to help with putting together error messages,
+
+// suport for concatenation of string-like things
+
+template<typename T>
+void append_to_string(std::string& s, const T& t)
+{
+    using text_types_list = mp11::mp_list<std::string, std::string_view, const char*, char>;
+
+    // look for things which are 'string like' so we can just append them.
+
+    if constexpr(std::is_same_v<mp11::mp_set_contains<text_types_list, std::remove_cv_t<T>>, mp11::mp_true>)
+    {
+        s += t;
+    }
+    else if constexpr(std::is_convertible_v<T, const char*>)
+    {
+        s +=t;
+    }
+    else if constexpr(std::is_arithmetic_v<T>)
+    {
+        // it's a number so convert it.
+
+        s += std::to_string(t);
+    }
+    else
+    {
+        // we don't know what to do with it.
+
+        throw std::invalid_argument("wrong type for 'catenate' function.");
+    }
+}
+
+// now, a function to concatenate a bunch of string-like things.
+
+template<typename... Ts>
+std::string catenate(Ts&&... ts)
+{
+    // let's use fold a expression
+    // (comma operator is cool...)
+
+    std::string x;
+    ( ... , append_to_string(x, std::forward<Ts>(ts)) );
+    return x;
+}
+
+#endif   /* ----- #IFNDEF COLLECTOR_UTILS_INC  ----- */
