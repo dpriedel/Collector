561ff11b8b38e2feed20705f1f4c985f90517339
David P. Riedel <davidp.riedel@gmail.com>
2021-05-04 14:44:23 -0400

    start working on downloads of statements and notes data sets

---
+++ src/FinancialStatementsAndNotes.cpp
@@ -0,0 +1,30 @@
+// =====================================================================================
+//
+//       Filename:  FinancialStatementsAndNotes.cpp
+//
+//    Description:  Module to manage download of SEC Financial Statement and Notes data files.
+//
+//        Version:  1.0
+//        Created:  05/04/2021 02:32:42 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+//
+
+//--------------------------------------------------------------------------------------
+//       Class:  FinancialStatementsAndNotes
+//      Method:  FinancialStatementsAndNotes
+// Description:  constructor
+//--------------------------------------------------------------------------------------
+
+#include "FinancialStatementsAndNotes.h"
+
+FinancialStatementsAndNotes::FinancialStatementsAndNotes ()
+{
+}  // -----  end of method FinancialStatementsAndNotes::FinancialStatementsAndNotes  (constructor)  ----- 
+
+
---
+++ src/FinancialStatementsAndNotes.h
@@ -0,0 +1,50 @@
+// =====================================================================================
+//
+//       Filename:  FinancialStatementsAndNotes.h
+//
+//    Description:  Module to manage download of SEC Financial Statement and Notes data files. 
+//
+//        Version:  1.0
+//        Created:  05/04/2021 02:30:37 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+
+
+// =====================================================================================
+//        Class:  FinancialStatementsAndNotes
+//  Description:  Module to manage download of SEC Financial Statement and Notes data files. 
+// =====================================================================================
+
+#ifndef  financialstatementsandnotes_INC
+#define  FinancialStatementsAndNotes_INC
+
+class FinancialStatementsAndNotes
+{
+    public:
+        // ====================  LIFECYCLE     ======================================= 
+        FinancialStatementsAndNotes ();                             // constructor 
+
+        // ====================  ACCESSORS     ======================================= 
+
+        // ====================  MUTATORS      ======================================= 
+
+        // ====================  OPERATORS     ======================================= 
+
+    protected:
+        // ====================  METHODS       ======================================= 
+
+        // ====================  DATA MEMBERS  ======================================= 
+
+    private:
+        // ====================  METHODS       ======================================= 
+
+        // ====================  DATA MEMBERS  ======================================= 
+
+}; // -----  end of class FinancialStatementsAndNotes  ----- 
+
+#endif   // ----- #ifndef FinancialStatementsAndNotes_INC  ----- 
